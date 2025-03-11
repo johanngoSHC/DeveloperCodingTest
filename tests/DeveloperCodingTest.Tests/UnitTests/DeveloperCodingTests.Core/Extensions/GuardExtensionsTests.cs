5357abb07e29444cc22d7c9c04a645366df2e18a
johanngoSHC <johanni_30@outlook.com>
2025-03-11 01:08:04 -0600

    Add GuardExtensions Unit Tests (#2)

---
+++ tests/DeveloperCodingTest.Tests/UnitTests/DeveloperCodingTests.Core/Extensions/GuardExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿namespace DeveloperCodingTest.Tests.UnitTests.DeveloperCodingTests.Core.Extensions;
+
+using Ardalis.GuardClauses;
+using DeveloperCodingTest.Core.Exceptions;
+using DeveloperCodingTest.Core.Extensions;
+using FakeItEasy;
+
+public class GuardExtensionsTests
+{
+    private readonly IGuardClause _fakeGuardClause;
+
+    public GuardExtensionsTests()
+    {
+        this._fakeGuardClause = A.Fake<IGuardClause>();
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void InvalidStoryId_ShouldThrowException_WhenIdIsZeroOrNegative(int invalidId)
+    {
+        // Act & Assert
+        Assert.Throws<InvalidStoryException>(() => _fakeGuardClause.InvalidStoryId(invalidId));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void InvalidStoryScore_ShouldThrowException_WhenScoreIsZeroOrNegative(int invalidScore)
+    {
+        // Act & Assert
+        Assert.Throws<InvalidStoryException>(() => _fakeGuardClause.InvalidStoryScore(invalidScore));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void InvalidStoryUri_ShouldThrowException_WhenUriIsNullEmptyOrWhitespace(string? invalidUri)
+    {
+        // Act & Assert
+        Assert.Throws<InvalidStoryException>(() => _fakeGuardClause.InvalidStoryUri(invalidUri));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData("2023-10-10 10:10:10")]
+    public void InvalidStoryDateTime_ShouldThrowException_WhenTimeIsInvalid(string? invalidTime)
+    {
+        // Act & Assert
+        Assert.Throws<InvalidStoryException>(() => _fakeGuardClause.InvalidStoryDateTime(invalidTime));
+    }
+}

b7fba62b99b4f3bd3420e4cdd684207b0bbf82db
johanngoSHC <johanni_30@outlook.com>
2025-03-11 17:05:58 -0600

    Add StoryService for Ranking Stories
    * Add GuardExtensions Unit Tests
    
    * Add StoryService with RankingNStories Functionallity
    
    * Add Unit Tests for StoryService

---
+++ tests/DeveloperCodingTest.Tests/UnitTests/DeveloperCodingTests.Core/Helper/SortingEngineHelperTests.cs
@@ -0,0 +1,35 @@
+﻿namespace DeveloperCodingTest.Tests.UnitTests.DeveloperCodingTests.Core.Helper;
+
+using System.Collections.Generic;
+using TestHelpers;
+using Xunit;
+using DeveloperCodingTest.Core.Helpers;
+
+public class SortEngineHelperTests
+{
+    private const int Count = 500;
+
+    [Theory]
+    [InlineData(10)]
+    [InlineData(50)]
+    [InlineData(100)]
+    [InlineData(500)]
+    public void RankNStories_ShouldReturnTopNStoriesOrderedByScoreDescending(int topN)
+    {
+        // Arrange
+        var stories = TestDataGenerator.GenerateStories(Count);
+
+        // Act
+        var result = SortEngineHelper.RankNStories(stories, topN);
+
+        // Assert
+        Assert.Equal(topN, result.Length);
+        Assert.True(AreStoriesOrderedByScoreDescending(result, stories));
+    }
+
+    private static bool AreStoriesOrderedByScoreDescending(int[] result, Dictionary<int, int> stories)
+    {
+        // Pair each story ID with the next story ID and check if the scores are in descending order
+        return result.Zip(result.Skip(1), (a, b) => stories[a] >= stories[b]).All(x => x);
+    }
+}

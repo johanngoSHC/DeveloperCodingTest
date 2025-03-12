42646e8efb26bf40b6c2dad19a99315b25acbc25
Johann Godinez <johanni_30@outlook.com>
2025-03-12 10:13:37 -0600

    Add Hacker News API Client

---
+++ src/DeveloperCodingTest.Infrastructure/HttpClient/HackerNewsPolicy.cs
@@ -0,0 +1,25 @@
+﻿namespace DeveloperCodingTest.Infrastructure.HttpClient;
+
+using Polly;
+using System;
+using System.Net.Http;
+
+
+public static class HackerNewsPolicy
+{
+    public static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        return Policy<HttpResponseMessage>
+            .Handle<HttpRequestException>()
+            .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+            .WaitAndRetryAsync(2, retryAttempt => TimeSpan.FromSeconds(Math.Pow(1.5, retryAttempt)));
+    }
+
+    public static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+    {
+        return Policy<HttpResponseMessage>
+            .Handle<HttpRequestException>()
+            .OrResult(msg => msg.StatusCode >= System.Net.HttpStatusCode.InternalServerError)
+            .CircuitBreakerAsync(5, TimeSpan.FromMinutes(1));
+    }
+}
